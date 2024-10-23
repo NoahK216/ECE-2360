e046ed6a70f4a5367f700d385869c1e978f0a401
acou12 <51276518+acou12@users.noreply.github.com>
2024-10-23 19:32:53 -0400

    Finish lesson 4.3 (joystick to hue)

--- lab4/5.C_JoystickLedStrip/5.C_JoystickLedStrip.ino
+++ lab4/5.C_JoystickLedStrip/5.C_JoystickLedStrip.ino
@@ -4,9 +4,10 @@
 #define yAxis 35 //joystick y
 #define btn 33 //joystick button
 
-#define AXIS_CENTER 2047.5 // (4095+0)/2
+// #define AXIS_CENTER 2047.5 // (4095+0)/2
+#define AXIS_CENTER 3000.0 // (4095+0)/2
 
-#define LED_PIN 14 // NeoPixel LED strip
+#define LED_PIN 26 // NeoPixel LED strip
 #define NUM_LEDS 8 // Number of LEDs
 
 // Create an instance of the Adafruit_NeoPixel class
@@ -15,7 +16,7 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ
 
 double heading(double x, double y) {
   double head = atan2(y, x); // Slope Y, Slope X
-  return head * (180/M_PI) + 180;
+  return head * (180.0/M_PI) + 180.0;
 }
 
 void setup() {
@@ -29,6 +30,9 @@ void setup() {
   strip.show(); // Set initial color to black
 }
 
+double smoothlyChangingHue = 0;
+double smoothlyChangingBrightness = 0;
+
 void loop() {
   // Read the analog value for x,y axis
   int xValue = analogRead(xAxis);
@@ -37,16 +41,33 @@ void loop() {
   int adjustedX = xValue - AXIS_CENTER;
   int adjustedY = yValue - AXIS_CENTER;
   double degrees = heading(adjustedX, adjustedY);
+  double dist = sqrt((double)(adjustedX * adjustedX + adjustedY * adjustedY));
 
   // Read the digital value from the button pin
   int btnValue = digitalRead(btn);
   
   // print out the values
-  Serial.printf("Joystick value is %d , %d , %lf \n",xValue, yValue, degrees);
+  Serial.printf("Joystick value is %d , %d , %lf, %lf\n",xValue, yValue, degrees, dist);
 
-  //strip.ColorHSV(degrees);
-  strip.Color(255, 255, 255);
+  double currentHue = degrees / 360.0 * 65535.0;
+  while (currentHue - smoothlyChangingHue > 65535.0 / 2.0) {
+    currentHue -= 65535.0;
+  }
+  while (smoothlyChangingHue - currentHue > 65535.0 / 2.0) {
+    currentHue += 65535.0;
+  }
+
+  double currentBrightness = dist >= 1000.0 ? 120.0 : 0.0;
+
+  smoothlyChangingHue += (currentHue - smoothlyChangingHue) * 0.1;
+  smoothlyChangingBrightness += (currentBrightness - smoothlyChangingBrightness) * 0.1;
+
+  int color = strip.ColorHSV((int)(smoothlyChangingHue), 255, (int)(smoothlyChangingBrightness));
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    strip.setPixelColor(i, color);
+  }
   strip.show(); // Update the LED strip with the new colors
   
-  delay(150);  // Delay between reads
+  delay(15);  // Delay between reads
 }
